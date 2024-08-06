0d46fe30cfe28ef59fb860d75171a4d00260d75b
Shex003 <eng.osman003@gmail.com>
2024-08-06 16:01:54 +0200

    navBar-registerForm-formik-submitForm

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-import style from './Navbar.module.css'
+import style from './Navbar.module.css';
+import img from '../../assets/img/logo.jpg';
+import {NavLink} from 'react-router-dom';
 
 export default function Navbar() {
   return <>
-    <div>Navbar</div>
+  <nav className='bg-slate-300 shadow-sm py-4'>
+  <div className='flex justify-between lg:items-center flex-col  lg:flex-row'>
+  <div className='logo flex flex-col lg:flex-row lg:items-center'>
+  <img scr={img} width={110} alt='frech-card'/>
+  <ul className='flex flex-col lg:flex-row'>
+    <li className='px-3 py-2'> <NavLink to={''}>Home</NavLink> </li>
+    <li className='px-3 py-2'> <NavLink to={'brands'}>Brands</NavLink> </li>
+    <li className='px-3 py-2'> <NavLink to={'products'}>Products</NavLink> </li>
+    <li className='px-3 py-2'> <NavLink to={'carts'}>Carts</NavLink> </li>
+  </ul>
+  </div>
+
+  <div className='social'>
+    <ul className='flex flex-col lg:flex-row lg:items-center'>
+    <li className='px-2'><NavLink to={'register'}>Register</NavLink></li>
+    <li className='px-2'><NavLink to={'login'}>Login</NavLink></li>
+    <li className='px-2'><NavLink to={'logout'}>Logout</NavLink></li>
+    <li>
+      <i className='fab px-2 fa-facebook'></i>
+      <i className='fab px-2 fa-youtube'></i>
+      <i className='fab px-2 fa-instagram'></i>
+    </li>
+    </ul>
+  </div>
+
+  </div>
+  </nav>
+    
     </>
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,86 @@
 import React from 'react';
 import style from './Register.module.css'
+import { useFormik } from 'formik';
 
 export default function Register() {
+
+  function handleRegister(param){
+    console.log('register', param);
+  }
+let formik = useFormik({
+      initialValues:{
+        name: '',
+        email: '',
+        password: '',
+        rePassword: '',
+        phone: ''
+    },
+    onSubmit:handleRegister
+})
+
   return <>
-    <div>Register</div>
+    <div class="bg-gray-100 flex h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
+        <div class="bg-white shadow-md rounded-md p-6">
+
+            {/* <img class="mx-auto h-12 w-auto" src="https://www.svgrepo.com/show/499664/user-happy.svg" alt="" /> */}
+
+            <h2 class="my-3 text-center text-3xl font-bold tracking-tight text-green-600">
+                Register Now
+            </h2>
+
+
+            <form onSubmit={formik.handleSubmit} class="space-y-6" method="POST">
+
+                <div>
+                    <label for="your-name" class="block text-sm font-medium text-gray-700">Username</label>
+                    <div class="mt-1">
+                        <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.name} name='name' id='your-name' type="text" required
+                            class="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm" />
+                    </div>
+                </div>
+
+                <div>
+                    <label for="your-email" class="block text-sm font-medium text-gray-700">Email</label>
+                    <div class="mt-1">
+                        <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} name='email' id='your-email' type="email" autocomplete="email-address" required
+                            class="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm" />
+                    </div>
+                </div>
+
+                <div>
+                    <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                    <div class="mt-1">
+                        <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.password} name='password' id='password' type="password" autocomplete="password" required
+                            class="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm" />
+                    </div>
+                </div>
+
+                <div>
+                    <label for="confirm-password" class="block text-sm font-medium text-gray-700">Confirm Password</label>
+                    <div class="mt-1">
+                        <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.rePassword} name='rePassword' id='confirm-password' type="password" autocomplete="confirm-password" required
+                            class="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm" />
+                    </div>
+                </div>
+
+                <div>
+                    <label for="phone" class="block text-sm font-medium text-gray-700">Phone</label>
+                    <div class="mt-1">
+                        <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.phone} name='phone' id='phone' type="tel" autocomplete="confirm-password" required
+                            class="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm" />
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md border border-transparent bg-green-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2">Register
+                        Account
+                        </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
     </>
 }
